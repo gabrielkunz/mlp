18e3e41c09ea1617dbd69a55a3bc5920d547bdb1
Gabriel Kunz <GabrielKunz97@gmail.com>
2021-06-14 19:06:56 -0300

    refactor(mlp): change mlp to MLP class

--- mlp.py
+++ mlp.py
@@ -1,44 +1,31 @@
 import numpy as np
 from perceptron import Perceptron
 
-def fit(mlp, X, target_prediction, epochs = 100):
-    # Perform the fit outside Perceptron class since the epoch is no applied on
-    # the entire net and the error does not come from a single neuron
+class MLP:
+    def __init__(self, X, hidden_layers = 1, alpha = 0.1):
+        self.p = Perceptron(X.shape[1], alpha)
 
-    # Add bias column
-    X = np.c_[X, np.ones((X.shape[0]))]
+    def fit(self, X, target_prediction, epochs = 100):
+        # Perform the fit outside Perceptron class since the epoch is no applied on
+        # the entire net and the error does not come from a single neuron
 
-    # Loop the epochs
-    for epoch in np.arange(0, epochs):
-        # Loop each data input
-        for (x, target) in zip(X, target_prediction):
-            # Weighted input
-            weighted_x = np.dot(x, mlp.weight)
+        # Add bias column
+        X = np.c_[X, np.ones((X.shape[0]))]
 
-            # Get the prediction
-            p = mlp.activation(weighted_x)
+        # Loop the epochs
+        for epoch in np.arange(0, epochs):
+            # Loop each data input
+            for (x, target) in zip(X, target_prediction):
+                # Weighted input
+                weighted_x = np.dot(x, self.p.weight)
 
-            # Update the weight based on the error, if the error is 0, the weight will remain the same
-            if p != target:
-                error = p - target
-                mlp.weight += -mlp.alpha * x * error
+                # Get the prediction
+                p = self.p.activation(weighted_x)
 
-X_and = np.array([[0,0],
-                  [0,1],
-                  [1,0],
-                  [1,1]])
-y_and = np.array([[0],
-                  [0],
-                  [0],
-                  [1]])
+                # Update the weight based on the error, if the error is 0, the weight will remain the same
+                if p != target:
+                    error = p - target
+                    self.p.weight += -self.p.alpha * x * error
 
-# Define perceptron AND and train it
-print("Training perceptron for AND dataset...")
-mlp = Perceptron(X_and.shape[1], alpha = 0.1)
-fit(mlp, X_and, y_and, epochs = 1000)
-
-# Display test result after training
-print("Testing perceptron after training...")
-for (x, target) in zip(X_and, y_and):
-	prediction = mlp.predict(x)
-	print("Input = {}, Target = {}, Prediction = {}".format(x, target[0], prediction))
+    def predict(self, X):
+       return self.p.predict(X)
